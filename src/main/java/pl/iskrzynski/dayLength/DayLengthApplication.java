f0c82223468697d5ce1ad7f72e2374dda027566a
SI <szymon@iskrzynski.pl>
2022-08-16 11:56:13 +0200

    deploy test2

--- src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
+++ src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
@@ -2,10 +2,16 @@ package pl.iskrzynski.dayLength;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
 public class DayLengthApplication {
-
+	@GetMapping("")
+	public String dayLength(){
+		return "24h";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DayLengthApplication.class, args);
 	}
