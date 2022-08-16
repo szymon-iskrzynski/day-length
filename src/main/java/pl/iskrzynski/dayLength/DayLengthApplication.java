2e1adb6dea5bd73c1717447698564a69382d6b2c
SI <szymon@iskrzynski.pl>
2022-08-16 23:26:07 +0200

    Aded congroller

--- src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
+++ src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
@@ -6,14 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/")
 public class DayLengthApplication {
-	@GetMapping("")
-	public String dayLength(){
-		return "24h";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(DayLengthApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayLengthApplication.class, args);
+    }
 
 }
