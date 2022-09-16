aca94289a4d396f4f910c4998afd086c4ca06b14
SI <szymon@iskrzynski.pl>
2022-09-16 14:13:44 +0200

    fix pom file

--- src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
+++ src/main/java/pl/iskrzynski/dayLength/DayLengthApplication.java
@@ -2,12 +2,19 @@ package pl.iskrzynski.dayLength;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DayLengthApplication {
+public class DayLengthApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DayLengthApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder){
+        return builder.sources(DayLengthApplication.class);
     }
 
 }
